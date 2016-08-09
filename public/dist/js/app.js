e3226661bb50620a99068b65d9e6a32242bb1bbd
Julio Kim <julio@playground2016.cafe24.com>
2016-08-10 06:30:48 +0900

    dist deleted...

--- public/dist/js/app.js
+++
@@ -1,1 +0,0 @@
-requirejs.config({baseUrl:"js",paths:{app:"app",lib:"lib",jquery:"http://code.jquery.com/jquery-1.11.2.min",underscore:"lib/underscore",handlebars:"lib/handlebars"},shim:{handlebars:{exports:"handlebars"}}}),requirejs(["app/main"]);
--- public/dist/js/app/main.js
+++
@@ -1,1 +0,0 @@
-define(function(e){var a=e("app/helloworld");a.narrator(),a.greeting(),a.msg(),e(["app/template"],function(e){e.underscoreTemplate("underscore"),e.handlebarsTemplate({name:"Handlebars"})})});
